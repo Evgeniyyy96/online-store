40a680681df43ed4d7348595cc49d9a75424b2e8
Evgeniyyy96 <evgeniytovstokoriy96@gmail.com>
2022-02-23 14:57:40 +0200

    Step-2

--- components/Test.vue
+++ components/Test.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    test
+    <h1>{{ message }}</h1>
+    <p>Count is: {{ counter.count }}</p>
   </div>
 </template>
 
@@ -9,6 +10,19 @@ export default {
   name: 'NameComponent',
   data () {
     return {}
+  }
+}
+</script>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello World',
+      counter: {                               // НЕ МОГУ ПОНЯТЬ ДЛЯ ЧЕГО НУЖНЫ 22 и 23 строка ( без них не работает )
+        count: 0
+      }
+    }
   }
 }
 </script>
